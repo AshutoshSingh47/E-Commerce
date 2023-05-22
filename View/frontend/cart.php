46fe11e0c7aaac1b4aad5b924da0bfa8a3a90d32
AshutoshSingh47 <ashutoshsingh.iimt@gmail.com>
2023-05-22 20:53:06 +0530

    created cart page and added css styles to it
    added the select tag instead of raw data in quantity

--- View/frontend/cart.php
+++ View/frontend/cart.php
@@ -26,7 +26,7 @@
                 <table>
                     <thead>
                         <tr>
-                            <th>Item</th>
+                            <th id="item">Item</th>
                             <th>Price</th>
                             <th>Quantity</th>
                             <th>Total Price</th>
@@ -36,25 +36,43 @@
                         <?php
                         while ($data = $result->fetch_assoc()) {
                             $pID[$rows] = $data["pID"];
-                            $sql = "SELECT ProductImage, ProductName, Price FROM adminproducts WHERE pID='$pID[$rows]'";
-                            $result2 = $conn->query($sql);?>
-                            <?php while($data2 = $result2->fetch_assoc()){ ?>
-                            <tr>
-                                <td><img src="<?=BASE_URI?>View/images/<?=$data2["ProductImage"]?>"></img><span><?=$data2["ProductName"]?></span></td>
-                                <td><?=$data2["Price"]?></td>
-                                <td><?=$data["Quantity"]?></td>
-                                <td><?=$data["TotalPrice"]?></td>
-                            </tr>
-                            <?php }$rows--;?>
-                       <?php } ?>
+                            $sql = "SELECT ProductImage, ProductName, Price, Quantity FROM adminproducts WHERE pID='$pID[$rows]'";
+                            $result2 = $conn->query($sql); ?>
+                            <?php while ($data2 = $result2->fetch_assoc()) { ?>
+                                <tr>
+                                    <td id="product-image">
+                                        <img id="item-image" src="<?= BASE_URI ?>View/images/<?= $data2["ProductImage"] ?>">
+                                        <div id="item-name">
+                                            <?= $data2["ProductName"] ?>
+                                        </div></img>
+                                    </td>
+                                    <td>
+                                        $<?= $data2["Price"] ?>.00
+                                    </td>
+                                    <td id="product-quantity">
+                                        <select name="quantity" id="quantity" placeholder="3">
+                                        <option value=""><?= $data["Quantity"] ?></option>
+                                        <?php for ($i = 1; $i <= $data2["Quantity"]; $i++) { ?>
+                                            <option value="<?= $i ?>"><?= $i ?></option>
+                                        <?php } ?>
+                                        </select>
+                                    </td>
+                                    <td id="product-total-price">$
+                                        <?= $data["TotalPrice"] ?>.00
+                                    </td>
+                                </tr>
+                            <?php }
+                            $rows--; ?>
+                        <?php } ?>
                     </tbody>
                 </table>
             </div>
             <div id="cart-total">
 
             </div>
+            <?php include("../E-Commerce/View/frontend/footer.php"); ?>
         </div>
-        <?php include("../E-Commerce/View/frontend/footer.php"); ?>
+
     </body>
 
     </html>
