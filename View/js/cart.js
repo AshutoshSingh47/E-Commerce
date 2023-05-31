0e1072fd668e41643b9dedbb526bd545b673e1db
AshutoshSingh47 <ashutoshsingh.iimt@gmail.com>
2023-05-31 20:53:15 +0530

    Formatted code

--- View/js/cart.js
+++ View/js/cart.js
@@ -1,38 +1,36 @@
-$(document).ready(function(){
-    let length=$(".quantity").length;
-    for(let i=0;i<length;i++)
-    {
-        $(".quantity").eq(i).on("change",function(){
-            let quantity=$(".quantity").eq(i).val();
-            let productid=$(".quantity").eq(i).attr("data-productid");
-            let customerid=$(".quantity").eq(i).attr("data-customerid");
+$(document).ready(function () {
+  let length = $(".quantity").length;
+  for (let i = 0; i < length; i++) {
+    $(".quantity")
+      .eq(i)
+      .on("change", function () {
+        let quantity = $(".quantity").eq(i).val();
+        let productid = $(".quantity").eq(i).attr("data-productid");
+        let customerid = $(".quantity").eq(i).attr("data-customerid");
 
-            getValue(productid,customerid,quantity);
-        });
-    }
+        getValue(productid, customerid, quantity);
+      });
+  }
 });
 
-function getValue(productid,customerid,quantity) {
-  let count=1;
-    $.ajax({
-      url: "http://localhost/E-Commerce/addtocart",
-      type: "POST",
-      data: {
-        productid: productid,
-        customerid: customerid,
-        quantity:quantity
-      },
-      success: function (data) {
-        $("#update-cart").on("click",function(){
-          location.reload();
-          if(count==1){
-            alert(data);
-            exit();
-          }
-      })
-      },
-    });
-  }
-
-
-  
+function getValue(productid, customerid, quantity) {
+  let count = 1;
+  $.ajax({
+    url: "http://localhost/E-Commerce/addtocart",
+    type: "POST",
+    data: {
+      productid: productid,
+      customerid: customerid,
+      quantity: quantity,
+    },
+    success: function (data) {
+      $("#update-cart").on("click", function () {
+        location.reload();
+        if (count == 1) {
+          alert(data);
+          exit();
+        }
+      });
+    },
+  });
+}
